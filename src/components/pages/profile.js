ef3902b38ce6d37d420d1b7943b495e1099b6a25
Jacob-Folley <jakebfolley@gmail.com>
2022-05-25 14:58:46 -0500

    Profile Page set up

--- src/components/pages/profile.js
+++ src/components/pages/profile.js
@@ -2,32 +2,42 @@ import React, { useEffect, useState } from 'react';
 import { useParms, useHistory } from 'react-router-dom';
 import { NavBar } from '../navbar/navbar'
 import { getProfile, getProfiles, createProfile, deleteProfile } from '../fetches/profile'
+import { getBlogPosts, deleteBlogPost } from '../fetches/blogpost';
 
 export const Profile = () => {
     const user = parseInt(localStorage.getItem("userId"))
     const history = useHistory()
 
-    const [profileInfo, setProfileInfo] = useState({})
-    const [profiles, setProfiles] = useState([])
+    // const [profileInfo, setProfileInfo] = useState({})
+    // const [profile, setProfile] = useState({})
+    // const [profiles, setProfiles] = useState([])
     const [form, updateForm] = useState({
         picture: "",
         summary: '',
         user: user
     });
 
-    //Gets the user profile
+    // //Fetches all the profiles
     // useEffect(() => {
-    //     getProfileInformation()
+    //     getProfiles().then((data) => {setProfiles(data)})
+    // },
+    // [])
+
+    // useEffect(() => {
+    //     myProfile()
+    // },
+    // [])
+
+    //   // Gets the user profile
+    //   useEffect(() => {
+    //     let checkProfile = profiles.find((number)=> {
+    //         return number?.user?.id === user
+    //     })
+    //     console.log(checkProfile)
+    //     setProfileInfo(checkProfile)
     // },
     //     [])
-    
-    //Fetches all the profiles
-    useEffect(() => {
-        getProfiles().then((data) => {setProfiles(data)})
-    },
-    [])
 
-    
     const changeFormState = (domEvent) => {
         const copy = { ...form }
         copy[domEvent.target.name] = (domEvent.target.value)
@@ -52,32 +62,131 @@ export const Profile = () => {
         });
     }
 
-    //Function that fetches the profile
-    const getProfileInformation = () => {
-        getProfile(foundProfile?.id).then((data) => {
-            setProfileInfo(data)
-        })
+    // //Function that fetches the profile
+    // const myProfile = () => {
+    //     getProfile(profileInfo?.id).then((data) => {
+    //         setProfile(data)
+    //     })
+    // }
+
+
+    // let checkProfile = profiles.find((number)=> {
+    //     return number?.user?.id === user
+    // })
+
+    const [profile, setProfile] = useState({})
+    const [profiles, setProfiles] = useState([])
+    const [number, setNumber] = useState(0)
+    const [posts, setPosts] = useState([])
+    const [myposts, setMyPosts] = useState([])
+
+
+    useEffect(() => {
+        findProfile().then((data) => { setProfile(data) })
+    },
+        [number])
+
+    useEffect(() => {
+        getProfiles().then((data) => { setProfiles(data) })
+    },
+        [])
+
+    useEffect(() => {
+        setNumber(profiles.find((profile) => {
+            return profile?.user?.id === user
+        }))
+    },
+        [profiles])
+
+    const findProfile = () => {
+        return getProfile(number?.id)
     }
 
-    const foundProfile = profiles.find((number) => {
-        return user == number.user?.id
-    })
+    useEffect(
+        () => {
+            blogPostings()
+        },
+        []
+    )
 
-    let checkProfile = profiles.find((number)=> {
-        return number.user?.id == user
-    })
+    useEffect(
+        () => {
+            setMyPosts(posts.filter((post) => {
+                return post.user.id == user
+            })
+            )
+        },
+        [posts]
+    )
+
+    const blogPostings = () => {
+        getBlogPosts()
+            .then((data) => {
+                setPosts(data)
+            })
+    }
+
 
 
     return (
         <>
             <NavBar />
-            
-            {/* {getProfileInformation()} */}
-            {checkProfile?.summary ? 
+            { }
+            {profile?.summary ?
                 <>
-                <h1>"Profile is there"</h1>
-                <img src={"http://localhost:8000" + checkProfile.picture} />
-                <p>{checkProfile.summary}</p>
+                    <section className="section is-small">
+                        <div className="columns is-variable is-1-mobile is-0-tablet is-3-desktop is-8-widescreen is-2-fullhd is-multiline">
+                            <div className="column is-one-fifth">
+                                <div class="card">
+                                    <div class="card-image">
+                                        <figure class="image is-4by3">
+                                            <img src={"http://localhost:8000" + profile?.picture} />
+                                        </figure>
+                                    </div>
+                                    <div class="card-content">
+                                        <div class="media">
+                                            <div class="media-content">
+                                                <p class="title is-4">{profile?.user?.first_name + " " + profile?.user?.last_name}</p>
+                                                <p class="subtitle is-6">{"@" + profile?.user?.username}</p>
+                                            </div>
+                                        </div>
+
+                                        <div class="content">
+                                            {profile?.summary}
+                                            <br />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="container">
+                                <section className="section is-small">
+                                    <div className="columns is-centered is-multiline">
+                                        {
+                                            myposts.map((post) => {
+                                                return <>
+                                                    <div className="column is-one-quarter content is-small is-mobile">
+                                                        <div className="box">
+                                                            <img className="img" src={"http://localhost:8000" + post.picture} onClick={() => { history.push(`/allposts/${post.id}`) }} />
+                                                            <h1 className="postLink content is-medium" onClick={() => { history.push(`/allposts/${post.id}`) }}>{post.title}</h1>
+                                                            <p></p>
+                                                            <div className="columns is-centered">
+                                                                <button type="submit" className="button is-small is-danger is-light" onClick={(e) => {
+                                                                    e.preventDefault()
+                                                                    deleteBlogPost(post.id).then(blogPostings)
+                                                                }}>Delete</button>
+                                                                <button className="button is-small is-info is-light">Edit</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </>
+                                            })
+                                        }
+                                    </div>
+                                </section>
+                            </div>
+                        </div>
+                    </section>
                 </>
                 :
                 <>
@@ -85,7 +194,7 @@ export const Profile = () => {
                     <form>
                         <input type="file" name="picture" onChange={createImageString}></input>
                         <textarea name="summary" onChange={changeFormState}></textarea>
-                        <button type="submit" onClick={(e)=>{
+                        <button type="submit" onClick={(e) => {
                             e.preventDefault();
                             const blogger = {
                                 user: form.user,
@@ -93,7 +202,7 @@ export const Profile = () => {
                                 summary: form.summary
                             }
                             createProfile(blogger)
-                            .then(getProfileInformation())
+                                .then(findProfile())
                         }}>Create Profile</button>
                     </form>
                 </>
